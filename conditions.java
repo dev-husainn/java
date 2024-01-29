1c179da842e5d7a66a8b02aae8173dac4aa84cc0
Husain Ahmad <157720720+dev-husainn@users.noreply.github.com>
2024-01-29 17:23:52 +0530

    conditions.java

--- conditions.java
+++ conditions.java
@@ -2,60 +2,60 @@
 
 /*Use 'if' to specify a block of code to be executed, if a specified condition is true*/
 
-// public class conditions {
-//     public static void main(String[] args){
-//         int num1 = 10;
-//         if(num1 > 9){
-//             System.out.println("Yes the num is greater than 9");
+ public class conditions {
+     public static void main(String[] args){
+         int num1 = 10;
+         if(num1 > 9){
+             System.out.println("Yes the num is greater than 9");
 
-//         }
+         }
 
-//     }
+     }
     
-// }
+ }
 /* ------------------------------------ - ----------------------------------- */
-// public class conditions {
-//     public static void main(String[] args){
-//         if(20 > 19){
-//             System.out.println("Yes the num is greater than 19");
+ public class conditions {
+     public static void main(String[] args){
+         if(20 > 19){
+             System.out.println("Yes the num is greater than 19");
 
-//         }
+         }
 
-//     }
+     }
     
-// }
+ }
 /* ------------------------------------ - ----------------------------------- */
-// public class conditions{
-//     public static void main(String[] args){
-//         int myNum1 = 500;
-//         int myNum2 = 700;
-//         if(myNum1 < myNum2){
-//             System.out.println(" Yeah myNum1 is less than myNum2");
+ public class conditions{
+     public static void main(String[] args){
+         int myNum1 = 500;
+         int myNum2 = 700;
+         if(myNum1 < myNum2){
+             System.out.println(" Yeah myNum1 is less than myNum2");
 
-//         }
-//     }
-// }
+         }
+     }
+ }
 
 /* --------------------------------- if else -------------------------------- */
 //Use else to specify a block of code to be executed, if the same condition is false.
 
-// public class conditions{
-//     public static void main(String[] args){
-//         int a = 999;
-//         int b = 888;
-//         if(a<b){
-//             System.out.println("True");
-//             }
+ public class conditions{
+     public static void main(String[] args){
+         int a = 999;
+         int b = 888;
+         if(a<b){
+             System.out.println("True");
+             }
             
-//             else{
-//                 System.out.println("False");
-//         }
-//     }
-// }
+             else{
+                 System.out.println("False");
+         }
+     }
+ }
 
 /* ------------------------------------ - ----------------------------------- */
 
-/*public class conditions{
+public class conditions{
     public static void main(String[] args){
         int time = 20;
         if(time > 15){
@@ -66,7 +66,7 @@
                 System.out.println("Good Evening");
         }
     }
-} */
+} 
 
 /* --------------------------------- else if -------------------------------- */
 /*Use else if to specify a new condition to test, if the first condition is false*/
