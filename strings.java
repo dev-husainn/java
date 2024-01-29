e66454a282b30f5aa01e26be28b09c1cdc5d0859
Husain Ahmad <157720720+dev-husainn@users.noreply.github.com>
2024-01-29 17:42:09 +0530

    strings.java

--- strings.java
+++ strings.java
@@ -4,45 +4,45 @@
 
 Example*/
 
-// public class strings{
-//     public static void main(String[]args){
-//         String greeting = "Hello Husain";
-//         System.out.println(greeting);
-//     }
-// }
+public class strings{
+    public static void main(String[]args){
+        String greeting = "Hello Husain";
+         System.out.println(greeting);
+    }
+ }
 
 /* ------------------------------ string lenght ----------------------------- */
 
 // the length of a string can be found with the length() method:
 
-// public class strings{
-//     public static void main(String[]args){
-//         String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//         System.out.println("The lenght of string is: " +letters.length());
-//     }
-// }
+ public class strings{
+    public static void main(String[]args){
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The lenght of string is: " +letters.length());
+    }
+ }
 
 /* ------------------------------------ x ----------------------------------- */
 /*There are many string methods available, for example toUpperCase() and toLowerCase()*/
-// public class strings {
-//     public static void main(String[] args) {
-//       String txt = "Hello World";
-//       System.out.println(txt.toUpperCase());
-//       System.out.println(txt.toLowerCase());
-//     }
-//   }
+public class strings {
+    public static void main(String[] args) {
+      String txt = "Hello World";
+      System.out.println(txt.toUpperCase());
+       System.out.println(txt.toLowerCase());
+     }
+   }
   
 /* ------------------------------------ x ----------------------------------- */
 
 /*Finding a Character in a String
 The indexOf() method returns the index (the position) of the first occurrence of a specified text in a string (including whitespace):*/
 
-// public class strings{
-//     public static void main(String[]args){
-//         String text = "Hello my name is husain ahmad ansari ";
-//         System.out.println(text.indexOf("husain"));
-//     }
-// }
+ public class strings{
+     public static void main(String[]args){
+         String text = "Hello my name is husain ahmad ansari ";
+         System.out.println(text.indexOf("husain"));
+     }
+ }
 
 
 
